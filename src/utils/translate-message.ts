7893d592ae2935d832a232f6f3acd7f87c3e6acc
Maike Mota <maike.mota@atos.net>
2019-08-21 19:12:12 -0300

    Muda a tradução da mensagem conforme a hora do dia
      - Entre 18h e 08h devolve uma composição usando 'Fortnite' como base
     - Fora deste período, mantem o que existe hoje

--- src/utils/translate-message.ts
+++ src/utils/translate-message.ts
@@ -1,5 +1,17 @@
-export const translateMessage = (message: string): string => {
-    return message.replace(/[0-9a-záàâãéèêíïóôõöúçñ]+/gi,
-        (string) => 'mi'.repeat(
-            Math.max(string.length / 2, 1)));
+import moment from 'moment-timezone';
+
+export const translateMessage = (timestamp: number, message: string): string => {
+
+    let translator = (string: string) => 'mi'.repeat(Math.max(string.length / 2, 1))
+
+    if (timestamp) {
+        let hourOfMessage = moment(timestamp).tz('America/Sao_Paulo').hour();
+        if (hourOfMessage >= 0 && hourOfMessage <= 8
+            || hourOfMessage >= 18 && hourOfMessage <= 23) {
+            translator = (string: string) => 
+                string.split(' ').map((value, index) => (index % 2 ? 'nite' : 'Fort')).join(' ');
+        }
+    }
+
+    return message.replace(/[0-9a-záàâãéèêíïóôõöúçñ]+/gi, translator);
 }
